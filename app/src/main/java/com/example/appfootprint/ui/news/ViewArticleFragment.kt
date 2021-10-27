2d64b0d66179cebf4368a4f8bc6593ea6a12f654
jelr20157@gmail.com <Jelr140301>
2021-10-27 14:30:04 -0500

    Obtención de Argumentos como Variable Global y obtener la noticia seleccionada y abrir en webView

--- app/src/main/java/com/example/appfootprint/ui/news/ViewArticleFragment.kt
+++ app/src/main/java/com/example/appfootprint/ui/news/ViewArticleFragment.kt
@@ -1,32 +1,40 @@
 package com.example.appfootprint.ui.news
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.example.appfootprint.MainActivity
-import com.example.appfootprint.R
+import com.example.appfootprint.databinding.FragmentViewArticleBinding
+import kotlinx.android.synthetic.main.fragment_view_article.*
 
 
 class ViewArticleFragment : Fragment() {
 
     lateinit var viewModel: BreakingNewsViewModel
+    private lateinit var mBinding: FragmentViewArticleBinding
+    val args: ViewArticleFragmentArgs by navArgs()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_article, container, false)
+        savedInstanceState: Bundle?): View? {
+        mBinding = FragmentViewArticleBinding.inflate(inflater, container, false)
+        return mBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as MainActivity).viewModel
+
+        val article = args.article
+        webView.apply {
+            webViewClient = WebViewClient()
+            loadUrl(article.url)
+        }
     }
 
 }
