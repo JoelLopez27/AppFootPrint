d3cf5e51925aac9684892a4552be4f327af0318f
jelr20157@gmail.com <Jelr140301>
2021-10-27 14:43:25 -0500

    setup de RecyclerView y agregado de onClickListener para la noticias, colocarlo en un bundle y adjuntarlo al componente de navegación

--- app/src/main/java/com/example/appfootprint/ui/savednews/SavedNewsFragment.kt
+++ app/src/main/java/com/example/appfootprint/ui/savednews/SavedNewsFragment.kt
@@ -5,25 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.appfootprint.MainActivity
+import com.example.appfootprint.R
+import com.example.appfootprint.adapters.NewsAdapter
 import com.example.appfootprint.databinding.FragmentSavedNewsBinding
 import com.example.appfootprint.ui.news.BreakingNewsViewModel
+import kotlinx.android.synthetic.main.fragment_breaking_news.*
+import kotlinx.android.synthetic.main.fragment_saved_news.*
 
 class savedNewsFragment : Fragment() {
 
     private lateinit var mBinding: FragmentSavedNewsBinding
     lateinit var viewModel: BreakingNewsViewModel
+    lateinit var newsAdapter: NewsAdapter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
         mBinding = FragmentSavedNewsBinding.inflate(inflater, container, false)
-
         return mBinding.root
 
     }
@@ -31,6 +33,26 @@ class savedNewsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as MainActivity).viewModel
+
+        setupRecyclerView()
+
+        newsAdapter.setOnItemClickListener {
+            val bundle = Bundle().apply {
+                putSerializable("article", it)
+            }
+            findNavController().navigate(
+                R.id.action_saved_News_to_viewArticleFragment,
+                bundle
+            )
+        }
     }
+    private fun setupRecyclerView(){
+        newsAdapter = NewsAdapter()
+        rvsavedNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
+
 
 }
