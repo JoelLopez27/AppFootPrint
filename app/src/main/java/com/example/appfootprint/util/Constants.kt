72e9ddcd767f8d75667b36917810ca250cba62f3
jelr20157@gmail.com <Jelr140301>
2021-11-05 00:05:28 -0600

    API KEY, HOST Y BASE URL de API Huella De Carbono

--- app/src/main/java/com/example/appfootprint/util/Constants.kt
+++ app/src/main/java/com/example/appfootprint/util/Constants.kt
@@ -2,7 +2,15 @@ package com.example.appfootprint.util
 
 class Constants {
     companion object{
+
+        //API NOTICIAS
         const val API_KEY = "65741713826a4c61ba9dd9ce13474306"
         const val BASE_URL = "https://newsapi.org"
+
+        //API HUELLA DE CARBONO
+
+        const val API_KEY_CARBONO = "023763f1c5msh5798c00ddbcd693p12c3efjsn9e25be762f73"
+        const val HOST = "carbonfootprint1.p.rapidapi.com"
+        const val BASE_URL_CARBONO = "https://carbonfootprint1.p.rapidapi.com/"
     }
 }
