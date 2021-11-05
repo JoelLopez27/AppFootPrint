6f50299ec7119ae95bdb24b3ad155b19c3d627d4
jelr20157@gmail.com <Jelr140301>
2021-11-05 00:42:49 -0600

    Initial commit

--- app/src/main/java/com/example/appfootprint/repository/NewsRepository.kt
+++ app/src/main/java/com/example/appfootprint/repository/NewsRepository.kt
@@ -3,7 +3,6 @@ package com.example.appfootprint.repository
 import com.example.appfootprint.api.RetrofitInstance
 import com.example.appfootprint.db.ArticleDatabase
 import com.example.appfootprint.models.Article
-import retrofit2.http.Query
 
 class NewsRepository(
     val db: ArticleDatabase
