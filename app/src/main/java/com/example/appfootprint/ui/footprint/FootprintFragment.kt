07a5122270b35e49007a18d9566549ba525e8d40
jelr20157@gmail.com <Jelr140301>
2021-11-10 21:05:20 -0600

    Buttons to Fragments Footprint

--- app/src/main/java/com/example/appfootprint/ui/footprint/FootprintFragment.kt
+++ app/src/main/java/com/example/appfootprint/ui/footprint/FootprintFragment.kt
@@ -17,9 +17,7 @@ import com.example.appfootprint.ui.footprint.calfootprint.FootprintCarTravelFrag
 
 class FootprintFragment : Fragment() {
 
-    private lateinit var footprintViewModel: FootprintViewModel
     private lateinit var mBinding: FragmentFootprintBinding
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,6 +33,32 @@ class FootprintFragment : Fragment() {
             it.findNavController().navigate(
                 R.id.action_nav_footprint_to_footprintCarTravelFragment)
         }
+
+        mBinding.button2.setOnClickListener {
+            it.findNavController().navigate(
+                R.id.action_nav_footprint_to_footprintFlightTravelFragment)
+        }
+        mBinding.button3.setOnClickListener {
+            it.findNavController().navigate(
+                R.id.action_nav_footprint_to_footprintBikeTravelFragment)
+        }
+        mBinding.button4.setOnClickListener {
+            it.findNavController().navigate(
+                R.id.action_nav_footprint_to_footprintTransitTravelFragment)
+        }
+
+        mBinding.button5.setOnClickListener {
+            it.findNavController().navigate(
+                R.id.action_nav_footprint_to_footprintHydroElectricFragment)
+        }
+
+        mBinding.button6.setOnClickListener {
+            it.findNavController().navigate(
+                R.id.action_nav_footprint_to_treesEquivalentFragment)
+        }
+
+
+
     }
 
 }
