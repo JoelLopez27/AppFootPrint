bd4cfbf46c23f5813b6439059d5755e6db221b31
jelr20157@gmail.com <Jelr140301>
2021-11-10 21:13:36 -0600

    Update ResultViewModel

--- app/src/main/java/com/example/appfootprint/ui/footprint/calfootprint/ResultViewModel.kt
+++ app/src/main/java/com/example/appfootprint/ui/footprint/calfootprint/ResultViewModel.kt
@@ -17,4 +17,30 @@ class ResultViewModel(
         val response = resultRepository.getResultCar(distanceKm, typeVehicle)
         resultLiveData.postValue(response)
     }
+
+    fun getResultFlight(distanceKm:String,typePlane:String) = viewModelScope.launch {
+        val response = resultRepository.getResultFlight(distanceKm, typePlane)
+        resultLiveData.postValue(response)
+    }
+
+    fun getResultBike(distanceKm:String,typeBike:String) = viewModelScope.launch {
+        val response = resultRepository.getResultBike(distanceKm, typeBike)
+        resultLiveData.postValue(response)
+    }
+
+    fun getResultTransit(distanceKm:String,typeTransit:String) = viewModelScope.launch {
+        val response = resultRepository.getResultTransit(distanceKm, typeTransit)
+        resultLiveData.postValue(response)
+    }
+
+    fun getResultHydroElectric(consumoKwh:String,location:String) = viewModelScope.launch {
+        val response = resultRepository.getResultHydroElectric(consumoKwh, location)
+        resultLiveData.postValue(response)
+    }
+
+    fun getResultTrees(peso:String,unidad:String) = viewModelScope.launch {
+        val response = resultRepository.getResultTrees(peso, unidad)
+        resultLiveData.postValue(response)
+    }
+
 }
