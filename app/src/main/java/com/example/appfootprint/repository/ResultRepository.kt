77978265b87e1404eca462fb9246a2ca1f879821
jelr20157@gmail.com <Jelr140301>
2021-11-10 21:12:53 -0600

    Add suspend functions to Result Repository

--- app/src/main/java/com/example/appfootprint/repository/ResultRepository.kt
+++ app/src/main/java/com/example/appfootprint/repository/ResultRepository.kt
@@ -1,7 +1,19 @@
 package com.example.appfootprint.repository
 
 import com.example.appfootprint.api.RetrofitInstance
+import retrofit2.http.Query
 
 class ResultRepository {
     suspend fun getResultCar(distanceKm:String,typeVehicle:String) = RetrofitInstance.apiCarbono.getResultCar(distanceKm, typeVehicle)
+
+    suspend fun getResultFlight(distanceKm:String,typePlane:String) = RetrofitInstance.apiCarbono.getResultFlight(distanceKm, typePlane)
+
+    suspend fun getResultBike(distanceKm:String,typeBike:String) = RetrofitInstance.apiCarbono.getResultBike(distanceKm, typeBike)
+
+    suspend fun getResultTransit(distanceKm:String,typeTransit:String) = RetrofitInstance.apiCarbono.getResultTransit(distanceKm, typeTransit)
+
+    suspend fun getResultHydroElectric(consumoKwh: String,country:String) = RetrofitInstance.apiCarbono.getResultHydroElectric(consumoKwh, country)
+
+    suspend fun getResultTrees(peso: String, unidad:String) = RetrofitInstance.apiCarbono.getResultTrees(peso, unidad)
+
 }
