988a3f4d262fdb2d8d209bdc17e20017eb3ac504
jelr20157@gmail.com <Jelr140301>
2021-11-10 21:05:46 -0600

    Fragment Calculate Footprint Travel HydroElectric

---
+++ app/src/main/java/com/example/appfootprint/ui/footprint/calfootprint/FootprintHydroElectricFragment.kt
@@ -0,0 +1,58 @@
+package com.example.appfootprint.ui.footprint.calfootprint
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.navigation.findNavController
+import com.example.appfootprint.MainActivity
+import com.example.appfootprint.R
+import com.example.appfootprint.databinding.FragmentFootprintHydroElectricBinding
+import com.example.appfootprint.databinding.FragmentFootprintTransitTravelBinding
+
+
+class FootprintHydroElectricFragment : Fragment() {
+
+    private lateinit var mBinding: FragmentFootprintHydroElectricBinding
+    private lateinit var viewModel: ResultViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        mBinding = FragmentFootprintHydroElectricBinding.inflate(inflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = (activity as MainActivity).viewModel3
+
+        val spinner = mBinding.spinner
+        ArrayAdapter.createFromResource(
+            requireContext(), R.array.location, android.R.layout.simple_spinner_dropdown_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+
+        mBinding.btnCalcular.setOnClickListener {
+            if (!mBinding.etKm.text.toString().isEmpty()&&!spinner.selectedItem.equals("-Seleccionar Pais/Continente-")) {
+                viewModel.getResultHydroElectric(
+                    mBinding.etKm.text.toString(),
+                    spinner.selectedItem.toString()
+                )
+                it.findNavController()
+                    .navigate(R.id.action_footprintHydroElectricFragment_to_fragmentResult)
+
+            } else {
+                Toast.makeText(activity, "Ingresar Dato Faltante", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+}
