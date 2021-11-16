8865d9780e087889af52bc6977562215871986de
jelr20157@gmail.com <Jelr140301>
2021-11-15 21:22:42 -0600

    Main Activity == recollectproviderFactory

--- app/src/main/java/com/example/appfootprint/MainActivity.kt
+++ app/src/main/java/com/example/appfootprint/MainActivity.kt
@@ -1,11 +1,7 @@
 package com.example.appfootprint
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
-import android.view.View
-import android.widget.Toast
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -16,30 +12,32 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.appfootprint.adapters.RecollectAdapter
 import com.example.appfootprint.databinding.ActivityMainBinding
 import com.example.appfootprint.db.ArticleDatabase
+import com.example.appfootprint.db.RecollectDatabase
 import com.example.appfootprint.repository.NewsRepository
+import com.example.appfootprint.repository.RecollectRepository
 import com.example.appfootprint.repository.ResultRepository
 import com.example.appfootprint.ui.footprint.calfootprint.ResultViewModel
 import com.example.appfootprint.ui.footprint.calfootprint.ViewModelProviderFactory
 import com.example.appfootprint.ui.home.HomeFragment
 import com.example.appfootprint.ui.news.BreakingNewsViewModel
 import com.example.appfootprint.ui.news.BreakingNewsViewModelProviderFactory
+import com.example.appfootprint.ui.recollect.AddRecollectViewModel
 import com.example.appfootprint.ui.recollect.RecollectFragment
 import com.example.appfootprint.ui.recollect.RecollectViewModel
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.view.*
-import kotlinx.android.synthetic.main.content_main.*
-import kotlinx.android.synthetic.main.fragment_breaking_news.*
-import kotlinx.android.synthetic.main.fragment_home.*
+import com.example.appfootprint.ui.recollect.RecollectViewModelProviderFactory
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var mBinding: ActivityMainBinding
     lateinit var viewModel: BreakingNewsViewModel
-    lateinit var viewModel2: RecollectViewModel
+    lateinit var viewModel2: AddRecollectViewModel
     lateinit var viewModel3: ResultViewModel
+    lateinit var recollectAdapter: RecollectAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,6 +47,11 @@ class MainActivity : AppCompatActivity() {
         val resultRepository = ResultRepository()
         val resultViewModelProviderFactory = ViewModelProviderFactory(resultRepository)
         viewModel3 = ViewModelProvider(this, resultViewModelProviderFactory).get(ResultViewModel::class.java)
+
+        val recollectRepository = RecollectRepository(RecollectDatabase(this))
+        val recollectProviderFactory = RecollectViewModelProviderFactory(recollectRepository)
+        viewModel2 = ViewModelProvider(this, recollectProviderFactory).get(AddRecollectViewModel::class.java)
+
 
         val newsRepository = NewsRepository(ArticleDatabase(this))
         val viewModelProviderFactory = BreakingNewsViewModelProviderFactory(newsRepository)
@@ -67,8 +70,9 @@ class MainActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
 
 
-        mBinding.appBarMain.fab.setOnClickListener { LaunchRecollectFragment() }
-
+            mBinding.appBarMain.fab.setOnClickListener {
+            LaunchRecollectFragment()
+        }
     }
 
     private fun LaunchRecollectFragment() {
@@ -82,6 +86,7 @@ class MainActivity : AppCompatActivity() {
                 .add(R.id.nav_host_fragment_content_main, recollectFragment, RecollectFragment::class.java.simpleName)
                 .commit()
         }
+
     }
 
 
