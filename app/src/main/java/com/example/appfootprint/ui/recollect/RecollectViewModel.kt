0adb34f958d589663e527dfcc923d1f90f972ce1
jelr20157@gmail.com <Jelr140301>
2021-11-15 21:26:55 -0600

    ViewModel RecollectFragment Eliminar recoleciones

--- app/src/main/java/com/example/appfootprint/ui/recollect/RecollectViewModel.kt
+++ app/src/main/java/com/example/appfootprint/ui/recollect/RecollectViewModel.kt
@@ -3,11 +3,21 @@ package com.example.appfootprint.ui.recollect
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.appfootprint.db.UserRecollect
+import com.example.appfootprint.repository.RecollectRepository
+import kotlinx.coroutines.launch
 
-class RecollectViewModel : ViewModel() {
+class RecollectViewModel(val recollectRepository: RecollectRepository) : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+
+   // fun getUpdateList(): LiveData<MutableList<UserRecollect>> = recollectRepository.getSavedRecollect()
+
+    fun deleteRecollectionData(userRecollect: UserRecollect) {
+        viewModelScope.launch {
+            val result: Int = recollectRepository.deleteRecollect(userRecollect)
+            if (result == 0)
+                throw IllegalAccessException("Invalid Recollect to delete")
+        }
     }
-    val text: LiveData<String> = _text
 }
