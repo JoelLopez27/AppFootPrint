4ea73b5536f5d9beeceb99c9c480739613df3ee2
jelr20157@gmail.com <Jelr140301>
2021-11-22 16:46:59 -0600

    Initial commit

--- app/src/main/java/com/example/appfootprint/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/appfootprint/ui/home/HomeViewModel.kt
@@ -3,11 +3,10 @@ package com.example.appfootprint.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.appfootprint.repository.RecollectRepository
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel(val recollectRepository: RecollectRepository) : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-       // value = "This is home Fragment"
-    }
-    val text: LiveData<String> = _text
+    fun getSavedRecollect() = recollectRepository.getSavedRecollect()
+
 }
