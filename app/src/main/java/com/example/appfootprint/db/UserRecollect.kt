92f648996814648a0c5bea57ffa70e0566b0174b
jelr20157@gmail.com <Jelr140301>
2021-11-22 16:51:40 -0600

    Update Entity

--- app/src/main/java/com/example/appfootprint/db/UserRecollect.kt
+++ app/src/main/java/com/example/appfootprint/db/UserRecollect.kt
@@ -7,9 +7,9 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "UserRecollect")
 data class UserRecollect(@PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id") var id: Int?,
                          @ColumnInfo(name = "material") var material: String,
-                         @ColumnInfo(name = "cantidad") var cantidad: String,
+                         @ColumnInfo(name = "cantidad") var cantidad: Double,
                          @ColumnInfo(name = "fecha") var fecha: String,
-                         @ColumnInfo(name = "co2") var co2: String) {
+                         @ColumnInfo(name = "co2") var co2: Double) {
 
-    constructor() : this(null, "","", "", "")
+    constructor() : this(null, "",0.0, "", 0.0)
 }
