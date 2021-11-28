e2a2113632fec02b1659297c901bb3b57619bd92
jelr20157@gmail.com <Jelr140301>
2021-11-28 01:20:41 -0600

    Upload Image Google Account

--- app/src/main/java/com/example/appfootprint/ui/footprint/FootprintFragment.kt
+++ app/src/main/java/com/example/appfootprint/ui/footprint/FootprintFragment.kt
@@ -4,16 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.appfootprint.R
-import com.example.appfootprint.databinding.FragmentBreakingNewsBinding
 import com.example.appfootprint.databinding.FragmentFootprintBinding
-import com.example.appfootprint.ui.footprint.calfootprint.FootprintCarTravelFragment
+import com.google.firebase.auth.FirebaseAuth
 
 class FootprintFragment : Fragment() {
 
@@ -28,6 +25,12 @@ class FootprintFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        Glide.with(this)
+            .load(FirebaseAuth.getInstance().currentUser?.photoUrl)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .centerCrop()
+            .into(mBinding.imageView)
 
         mBinding.fab.setOnClickListener {
             it.findNavController()
