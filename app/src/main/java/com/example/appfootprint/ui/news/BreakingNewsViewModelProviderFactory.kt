25f216a68f0fc3ed3a1f7579b3c84080ceafeea9
jelr20157@gmail.com <Jelr140301>
2021-11-28 01:16:11 -0600

    Upload Provider Factory

--- app/src/main/java/com/example/appfootprint/ui/news/BreakingNewsViewModelProviderFactory.kt
+++ app/src/main/java/com/example/appfootprint/ui/news/BreakingNewsViewModelProviderFactory.kt
@@ -1,14 +1,16 @@
 package com.example.appfootprint.ui.news
 
+import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.appfootprint.repository.NewsRepository
 
 class BreakingNewsViewModelProviderFactory(
+    val app: Application,
     val newsRepository: NewsRepository
 ) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return BreakingNewsViewModel(newsRepository) as T
+        return BreakingNewsViewModel(app, newsRepository) as T
     }
 }
