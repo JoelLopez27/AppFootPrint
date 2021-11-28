7d446eeab0cca4dab091a7bce9c143821ff048cd
jelr20157@gmail.com <Jelr140301>
2021-11-28 01:15:48 -0600

    View Model Conexión o No a Internet

--- app/src/main/java/com/example/appfootprint/ui/news/BreakingNewsViewModel.kt
+++ app/src/main/java/com/example/appfootprint/ui/news/BreakingNewsViewModel.kt
@@ -1,18 +1,25 @@
 package com.example.appfootprint.ui.news
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.ConnectivityManager.*
+import android.net.NetworkCapabilities.*
+import android.os.Build
+import androidx.lifecycle.*
+import com.example.appfootprint.NewsApplication
 import com.example.appfootprint.models.Article
 import com.example.appfootprint.models.NewsResponse
 import com.example.appfootprint.repository.NewsRepository
 import com.example.appfootprint.util.Resource
 import kotlinx.coroutines.launch
 import retrofit2.Response
+import java.io.IOException
 
 class BreakingNewsViewModel(
+    app: Application,
     val newsRepository : NewsRepository
-) : ViewModel() {
+) : AndroidViewModel(app) {
 
     val breakingNews : MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
     var breakingNewsPage = 1
@@ -25,10 +32,7 @@ class BreakingNewsViewModel(
 
     fun getBreakingNews(selectedTopic: String, selectedLanguage: String, comeFisrt: String) =
         viewModelScope.launch {
-            breakingNews.postValue(Resource.Loading())
-            val response = newsRepository.getBreakingNews(selectedTopic, selectedLanguage, comeFisrt,
-            breakingNewsPage, breakingNewNumber)
-            breakingNews.postValue(handleBreakingNewsResponse(response))
+           safeBreakingNewsCall(selectedTopic, selectedLanguage, comeFisrt)
         }
 
     private fun handleBreakingNewsResponse(response: Response<NewsResponse>) : Resource<NewsResponse> {
@@ -59,4 +63,50 @@ class BreakingNewsViewModel(
         newsRepository.deleteArticle(article)
     }
 
+    fun getSizeRows(): LiveData<Int> = newsRepository.getSizeRows()
+
+
+    private suspend fun safeBreakingNewsCall(selectedTopic: String, selectedLanguage: String, comeFisrt: String){
+        breakingNews.postValue(Resource.Loading())
+        try {
+            if(siConexionInternet()){
+                val response = newsRepository.getBreakingNews(selectedTopic, selectedLanguage, comeFisrt,
+                    breakingNewsPage, breakingNewNumber)
+                breakingNews.postValue(handleBreakingNewsResponse(response))
+            }else{
+                breakingNews.postValue(Resource.Error("No hay Conexión a Internet"))
+            }
+        }catch (t: Throwable) {
+            when(t) {
+                is IOException -> breakingNews.postValue(Resource.Error("Internet Fallando"))
+                else -> breakingNews.postValue(Resource.Error("Error al Convertir"))
+            }
+        }
+    }
+
+    private fun siConexionInternet(): Boolean{
+            val connectivityManager = getApplication<NewsApplication>().getSystemService(
+                Context.CONNECTIVITY_SERVICE
+            ) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            val activeNetwork = connectivityManager.activeNetwork ?: return false
+            val capabilities = connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
+            return when{
+                    capabilities.hasTransport(TRANSPORT_WIFI) -> true
+                    capabilities.hasTransport(TRANSPORT_CELLULAR) -> true
+                    capabilities.hasTransport(TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+        }else{
+            connectivityManager.activeNetworkInfo?.run {
+                return when(type){
+                    TYPE_WIFI -> true
+                    TYPE_MOBILE -> true
+                    TYPE_ETHERNET -> true
+                    else -> false
+                }
+            }
+        }
+        return false
+    }
 }
